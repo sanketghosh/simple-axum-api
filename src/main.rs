f46bb761bfe540e61b6480508a0d694549aff0c0
sanket <code.sanket13@gmail.com>
2024-02-06 23:56:08 +0530

    hello2 endpoint added with dynamic routing capability

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,9 @@ use tokio::net::TcpListener;
 
 #[tokio::main]
 async fn main() {
-    let routes_hello = Router::new().route("/hello", get(hello_handler));
+    let routes_hello = Router::new()
+        .route("/hello", get(hello_handler))
+        .route("/hello2/:name", get(hello2_handler));
 
     // REGION: ---> Start Server
     let listener = TcpListener::bind("127.0.0.1:6969").await.unwrap();
@@ -35,6 +37,13 @@ async fn hello_handler(Query(params): Query<HelloParams>) -> impl IntoResponse {
     let name = params.name.as_deref().unwrap_or("world");
 
     Html(format!("Hello {name}."))
+}
+
+//e.g, `hello/Jane`
+async fn hello2_handler(Path(name): Path<String>) -> impl IntoResponse {
+    println!("->> {:<12} - handler_hello {name:?}", "HANDLER");
+
+    Html(format!("Hello2 {name}."))
 }
 
 // endregion: ---> handler hello
--- tests/quick_dev.rs
+++ tests/quick_dev.rs
@@ -3,6 +3,6 @@ use anyhow::{Ok, Result};
 #[tokio::test]
 async fn quick_dev() -> Result<()> {
     let hc = httpc_test::new_client("http://localhost:6969")?;
-    hc.do_get("/hello?name=John").await?.print().await?;
+    hc.do_get("/hello2/Jane").await?.print().await?;
     Ok(())
 }
