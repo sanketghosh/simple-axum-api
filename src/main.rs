cca9522a383359e9a8452094fbd0c2ec758dc1f6
sanket <code.sanket13@gmail.com>
2024-02-07 00:40:04 +0530

    add: static route

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,34 @@
 use axum::{
     extract::{Path, Query},
     response::{Html, IntoResponse},
-    routing::get,
+    routing::{get, get_service},
     Router,
 };
 use serde::Deserialize;
 use tokio::net::TcpListener;
+use tower_http::services::ServeDir;
 
 #[tokio::main]
 async fn main() {
-    let routes_hello = Router::new()
-        .route("/hello", get(hello_handler))
-        .route("/hello2/:name", get(hello2_handler));
+    let routes_all = Router::new().merge(routes_hello().fallback_service(routes_static()));
 
     // REGION: ---> Start Server
     let listener = TcpListener::bind("127.0.0.1:6969").await.unwrap();
     println!("Listening on {:?}", listener.local_addr());
-    axum::serve(listener, routes_hello.into_make_service())
+    axum::serve(listener, routes_all.into_make_service())
         .await
         .unwrap();
 
     // ENDREGION: ---> Start Server
 }
 
-// region: ---> handler hello
+// region: ---> Routes hello
+
+fn routes_hello() -> Router {
+    Router::new()
+        .route("/hello", get(hello_handler))
+        .route("/hello2/:name", get(hello2_handler))
+}
 
 #[derive(Debug, Deserialize)]
 struct HelloParams {
@@ -46,4 +51,9 @@ async fn hello2_handler(Path(name): Path<String>) -> impl IntoResponse {
     Html(format!("Hello2 {name}."))
 }
 
-// endregion: ---> handler hello
+// static routes
+fn routes_static() -> Router {
+    Router::new().nest_service("/", get_service(ServeDir::new("./")))
+}
+
+// endregion: ---> Routes hello
--- tests/quick_dev.rs
+++ tests/quick_dev.rs
@@ -4,5 +4,6 @@ use anyhow::{Ok, Result};
 async fn quick_dev() -> Result<()> {
     let hc = httpc_test::new_client("http://localhost:6969")?;
     hc.do_get("/hello2/Jane").await?.print().await?;
+    hc.do_get("/src/main.rs").await?.print().await?;
     Ok(())
 }
